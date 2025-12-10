56adb5505cacd7da0ab3360d2da1b5e2b8b798e7
Andrew-Rollins <72422826+Andrew-Rollins@users.noreply.github.com>
2025-12-10 14:39:25 -0600

    sub tests

--- test/test_app.py
+++ test/test_app.py
@@ -5,9 +5,16 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root/"src"))
 
 from app import add
+from app import sub
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_sub():
+    assert sub(3,2) == 1
+
+def test_sub2():
+    assert sub(3,2) != 0
