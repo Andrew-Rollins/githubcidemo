9adf9452e02d790d5487f2c91f8840f8d746aa8b
Andrew-Rollins <72422826+Andrew-Rollins@users.noreply.github.com>
2025-12-10 14:47:25 -0600

    consolidate tests and div

--- test/test_app.py
+++ test/test_app.py
@@ -4,27 +4,23 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root/"src"))
 
-from app import add
-from app import sub
-from app import mult
+from app import *
 
 def test_add():
     assert add(5, 6) == 11
-
-def test_add2():
     assert add(5, 6) != 10
 
 def test_sub():
     assert sub(3,2) == 1
-
-def test_sub2():
     assert sub(3,2) != 0
 
 def test_mult():
     assert mult(3,3) == 9
-
-def test_mult1():
     assert mult(0,3) == 0
+    assert mult(3,3) !=8
 
-def test_mult2():
-    assert mult (3,3) !=8
+def test_div():
+    assert div(0/3) == 0
+    assert div(0/3) != 1
+    assert div(3/0) == None
+    
